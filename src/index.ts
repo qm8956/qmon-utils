c7c25c5820f09294ee66a7b7429313f6e91ae6ca
qianming <qianming_521@126.com>
2024-09-27 19:09:35 +0800

    修改导出遗漏

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,19 @@ export {
 export {
   appendQueryParams,
 } from './url';
+export {
+  getTableHtml,
+  exportExcel,
+} from './excel';
+
+export {
+  tryit,
+  sleep,
+} from './async';
+
+export {
+  generateUUID,
+  str2json,
+  str2jsonByAsync,
+} from './string';
+
